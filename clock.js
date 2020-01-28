be53169734b489a47bc7eaf25c566ff6a9c5a5ce
ofkande <60219368+ofkande@users.noreply.github.com>
2020-01-28 22:27:09 +0900

    edit clockTitle

--- clock.js
+++ clock.js
@@ -1,14 +1,17 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1"),
+  clockHours = clockTitle.querySelector(".hours"),
+  clockMinutes = clockTitle.querySelector(".minutes"),
+  clockSeconds = clockTitle.querySelector(".seconds");
 
 function getTime() {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockHours.innerText = `${hours < 10 ? `0${hours}` : hours}:`;
+  clockMinutes.innerText = `${minutes < 10 ? `0${minutes}` : minutes}:`;
+  clockSeconds.innerText = `${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
